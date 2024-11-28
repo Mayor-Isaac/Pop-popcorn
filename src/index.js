796779cae0e7a6ad6bead8b85d60fdab15c0a37e
Mayor-Isaac <ogunyilekaferanmi@gmail.com>
2024-11-28 20:13:30 +0100

    Text Expander Challenge

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import StarRating from "./StarRating";
+import TextExpanderApp from "./TextExpander";
 // import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating
+    {/* <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
-    <StarRating size={24} color="red" className="test" defaultRating={3} />
+    <StarRating size={24} color="red" className="test" defaultRating={3} /> */}
+    <TextExpanderApp />
   </React.StrictMode>
 );
