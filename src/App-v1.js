7e993c97698be344e13b380f6de0a12ec52f40ff
Mayor-Isaac <ogunyilekaferanmi@gmail.com>
2024-12-01 22:01:48 +0100

    API added

--- src/App-v1.js
+++ src/App-v1.js
@@ -1,5 +1,5 @@
 import "./index.css";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 const tempMovieData = [
   {
@@ -51,19 +51,10 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-const KEY = "f84fc31d";
-
 export default function App() {
-  const [watched, setWatched] = useState([]);
-  const [movies, setMovies] = useState([]);
+  const [watched, setWatched] = useState(tempWatchedData);
+  const [movies, setMovies] = useState(tempMovieData);
 
-  useEffect(() => {
-    fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
-      .then((res) => res.json())
-      .then((data) => {
-        setMovies(data.Search);
-      });
-  }, []);
   return (
     <>
       <NavBar>
--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import StarRating from "./StarRating";
 import TextExpanderApp from "./TextExpander";
-// import App from "./App";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
+    <App />
     {/* <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
     <StarRating size={24} color="red" className="test" defaultRating={3} /> */}
-    <TextExpanderApp />
+    {/* <TextExpanderApp /> */}
   </React.StrictMode>
 );
